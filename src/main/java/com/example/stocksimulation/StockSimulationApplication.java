e41a43da2112bba0f3deddf45976f49d9e56616f
piedra-de-flor <xxspdlqjxx@naver.com>
2024-03-31 01:01:43 +0900

    Initial commit

---
+++ src/main/java/com/example/stocksimulation/StockSimulationApplication.java
@@ -0,0 +1,13 @@
+package com.example.stocksimulation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockSimulationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockSimulationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/stocksimulation/StockSimulationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.stocksimulation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockSimulationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
