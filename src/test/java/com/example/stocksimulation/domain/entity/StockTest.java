b2a31a7675c244b29abe4c8f5edb3118351a76f1
piedra-de-flor <xxspdlqjxx@naver.com>
2024-04-26 10:45:11 +0900

    test : stock 엔티티와 StockService에 대한 단위테스트 작성

--- src/test/java/com/example/stocksimulation/domain/entity/StockTest.java
+++ src/test/java/com/example/stocksimulation/domain/entity/StockTest.java
@@ -1,7 +1,18 @@
 package com.example.stocksimulation.domain.entity;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 class StockTest {
+    @Test
+    void 주식_가격_정보_업데이트_테스트() {
+        Stock stock = new Stock();
 
+        stock.updatePrice(1000);
+        assertThat(stock.getPrice()).isEqualTo(1000);
+
+        stock.updatePrice(5000);
+        assertThat(stock.getPrice()).isEqualTo(5000);
+    }
 }
--- src/test/java/com/example/stocksimulation/service/stock/StockServiceTest.java
+++ src/test/java/com/example/stocksimulation/service/stock/StockServiceTest.java
@@ -1,7 +1,69 @@
 package com.example.stocksimulation.service.stock;
 
-import static org.junit.jupiter.api.Assertions.*;
+import com.example.stocksimulation.domain.entity.Stock;
+import com.example.stocksimulation.dto.stock.StockDto;
+import com.example.stocksimulation.repository.StockRepository;
+import com.example.stocksimulation.service.support.WebSocketClientToServerHandler;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.test.util.ReflectionTestUtils;
 
-class StockServiceTest {
+import java.util.HashMap;
+import java.util.NoSuchElementException;
+import java.util.Optional;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+
+public class StockServiceTest {
+
+    @Mock
+    private StockRepository repository;
+    @Mock
+    private WebSocketClientToServerHandler socketServerHandler;
+    @InjectMocks
+    private StockService service;
+
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.openMocks(this);
+        ReflectionTestUtils.setField(socketServerHandler, "stocks", new HashMap<>());
+    }
+
+    @Test
+    public void testUpdateStockPrice() {
+        String stockCode = "AAPL";
+        int currentPrice = 100;
+        int newPrice = 150;
+
+        Stock stock = Stock.builder()
+                .code(stockCode)
+                .price(currentPrice)
+                .name("test")
+                .build();
+
+        StockDto stockDto = new StockDto(stockCode, newPrice, "test");
+
+        when(repository.findByCode(stockCode)).thenReturn(Optional.of(stock));
+
+        service.updateStockPrice(stockCode, newPrice);
+
+        verify(repository, times(1)).findByCode(stockCode);
+        verify(socketServerHandler, times(1)).sendStockDtoToAllClients(stockDto);
+        assertEquals(newPrice, stock.getPrice());
+    }
+
+    @Test
+    public void testUpdateStockPrice_StockNotFound() {
+        String stockCode = "AAPL";
+        int newPrice = 150;
+
+        when(repository.findByCode(stockCode)).thenReturn(Optional.empty());
+
+        Assertions.assertThrows(NoSuchElementException.class, () -> service.updateStockPrice(stockCode, newPrice));
+    }
 }
