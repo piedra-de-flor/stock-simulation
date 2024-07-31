7ea8d8f21b876a4629cdb6286c3fdecb99579047
piedra-de-flor <xxspdlqjxx@naver.com>
2024-07-31 14:32:42 +0900

    feat : trade 메서드에서의 동시성 문제 이유로 AccountRepository에 비관적 Lock 추가

--- src/main/java/com/example/stocksimulation/repository/AccountRepository.java
+++ src/main/java/com/example/stocksimulation/repository/AccountRepository.java
@@ -1,7 +1,14 @@
 package com.example.stocksimulation.repository;
 
 import com.example.stocksimulation.domain.entity.Account;
+import com.example.stocksimulation.domain.entity.Member;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+
+import java.util.Optional;
 
 public interface AccountRepository extends JpaRepository<Account, Long> {
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    Optional<Account> findByMember(Member member);
 }
